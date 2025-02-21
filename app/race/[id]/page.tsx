9043238fbf2dc7d6a89996b0cebb288b3f1733fb
federicotoluzzo <federico.toluzzo1@gmail.com>
2025-02-21 10:51:48 +0100

    fixed for eslint

--- app/race/[id]/page.tsx
+++ app/race/[id]/page.tsx
@@ -36,8 +36,9 @@ export default async function Page({ params }) {
     const id = (await params).id;
 
     try {
-        const url_data = await fetchData(baseURL + id);const data = await fetchData('https://apicanoavelocita.ficr.it/CAV/mpcache-10/get/result/' + id + '/KY/' + url_data.data[0].e[0].c0 + "/" + url_data.data[0].e[0].c1 + "/" + url_data.data[0].e[0].c2.substring(1) + "/" + url_data.data[0].e[0].c3); // prima parte fissa, poi result o startlist, poi ... , poi KY (fisso per kayak), c0, c1, c2 (troncato) e c3.
-        const heats = processHeatsData(data.data.data); // Assuming a separate function to process heats data
+        const url_data = await fetchData(baseURL + id);
+        //const data = await fetchData('https://apicanoavelocita.ficr.it/CAV/mpcache-10/get/result/' + id + '/KY/' + url_data.data[0].e[0].c0 + "/" + url_data.data[0].e[0].c1 + "/" + url_data.data[0].e[0].c2.substring(1) + "/" + url_data.data[0].e[0].c3); // prima parte fissa, poi result o startlist, poi ... , poi KY (fisso per kayak), c0, c1, c2 (troncato) e c3.
+        //const heats = processHeatsData(data.data.data); // Assuming a separate function to process heats data
         /*
         return (
             <div>
@@ -47,17 +48,17 @@ export default async function Page({ params }) {
             </div>
         );*/
 
-        let competitors = []
+        const competitors = []
 
         // tipo gara = d1_it
         // categoria = d3_it
         // distanza = d_it
 
-        for (let foo of url_data.data){
-            for (let bar of foo.e){
+        for (const foo of url_data.data){
+            for (const bar of foo.e){
                 console.log(bar)
                 const data = await fetchData('https://apicanoavelocita.ficr.it/CAV/mpcache-10/get/result/' + id + '/KY/' + bar.c0 + "/" + bar.c1 + "/" + bar.c2.substring(1) + "/" + bar.c3);
-                let thing = {data : "", raceName : ""}
+                const thing = {data : "", raceName : ""}
                 if (data !== undefined){
                     thing.data = processHeatsData(data.data.data);
                     thing.raceName = bar.d1_it + " " + bar.d3_it + " " + bar.d_it;
@@ -96,9 +97,9 @@ export default async function Page({ params }) {
  * @param data l'oggetto restituito
  */
 function processHeatsData(data) {
-    let heats: Competitor[][] = [];
+    const heats: Competitor[][] = [];
 
-    for (let athlete of data){
+    for (const athlete of data){
         if (heats[athlete.b - 1] === undefined){
             heats[athlete.b - 1] = [];
         }
