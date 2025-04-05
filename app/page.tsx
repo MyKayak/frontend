179ea2ea7549eed9fac1985cbd7f765568428770
federicotoluzzo <federico.toluzzo1@gmail.com>
2025-04-05 09:02:13 +0200

    1.0.5 Fixata la homepage, forse?

--- app/page.tsx
+++ app/page.tsx
@@ -2,39 +2,36 @@ import React from 'react'
 import YearSelector from "@/assets/YearSelector";
 
 interface Race{
-  CodicePub:string,
-  Description:string,
-  Place:string,
-  Data:string
+    CodicePub:string,
+    Description:string,
+    Place:string,
+    Data:string
 }
 
 async function getData(year:number):Promise<Race[]>{
-    let response = null;
-    try{
-        response = await fetch(`https://apimanvarie.ficr.it/VAR/mpcache-30/get/schedule/${year}/*/19`);
-    } catch {return null}
+    const response = await fetch(`https://apimanvarie.ficr.it/VAR/mpcache-30/get/schedule/${year}/*/19`);
     const data = await response.json();
     return data.data;
 }
 
 
 export default async function Page(){
-  const year = new Date().getFullYear();
-  const data = await getData(year);
-  return (
-      <div className="flex items-center flex-col">
-        <YearSelector defaultYear={year}></YearSelector>
-        <br></br>
-        {data.map(race => (
-            <a href={"/race/" + race.CodicePub} key={race.CodicePub} className="w-10/12  justify-self-center self-center items-center flex flex-col">
-              <button className="m-4 btn btn-soft btn w-full p-4 h-fit">
-                <div className="flex w-full flex-row justify-between items-center gap-4">
-                  <p>{race.Description}</p>
-                  <p>{race.Place}<br></br>{race.Data}</p>
-                </div>
-              </button>
-            </a>
-        ))}
-      </div>
-  );
+    const year = (new Date()).getFullYear();
+    const data = await getData(Number(year));
+    return (
+        <div className="flex items-center flex-col">
+            <YearSelector defaultYear={Number(year)}></YearSelector>
+            <br></br>
+            {data.map(race => (
+                <a href={"/race/" + race.CodicePub} key={race.CodicePub} className="w-10/12  justify-self-center self-center items-center flex flex-col">
+                    <button className="m-4 btn btn-soft btn w-full p-4 h-fit">
+                        <div className="flex w-full flex-row justify-between items-center gap-4">
+                            <p>{race.Description}</p>
+                            <p>{race.Place}<br></br>{race.Data}</p>
+                        </div>
+                    </button>
+                </a>
+            ))}
+        </div>
+    );
 }
