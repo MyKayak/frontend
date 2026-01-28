af4fcefdf2e2f0c747c608157970b900ab98a390
Federico Toluzzo <federico.toluzzo1@gmail.com>
2026-01-28 13:30:19 +0100

    2.1.0 added a cool dynamic navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Nav from "@/components/ui/nav";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +14,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700" rel="stylesheet" />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
+        <Nav />
         {children}
       </body>
     </html>
---
+++ components/ui/nav.tsx
@@ -0,0 +1,59 @@
+"use client"
+import { CircleUserRound } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+
+interface Button {
+  label:string,
+  path:string,
+  isActive:boolean
+}
+
+const Nav = () => {
+  const path = usePathname();
+
+  const buttons:Button[] = [
+    {
+      "label":"MyKayak",
+      "path":"/",
+      "isActive":false
+    },
+    {
+      "label":"Gare",
+      "path":"/races",
+      "isActive":false
+    },
+    {
+      "label":"Atleti",
+      "path":"/athletes",
+      "isActive":false
+    },
+    {
+      "label":"Società",
+      "path":"/teams",
+      "isActive":false
+    },
+  ];
+
+  buttons.forEach((button)=>{
+    if(button.path === path) {
+      button.isActive = true;
+    }
+  });
+
+  return (
+    <nav className="fixed top-0 z-20 flex items-center justify-center w-full p-4 font-medium transition-all">
+        <div className="flex gap-2 p-2 bg-sky-200/20 w-fit rounded-full backdrop-blur-xl backdropt-brightness-80%">
+          {buttons.map((button)=>(
+            <a className={`p-4 rounded-full hover:bg-sky-200/10 transition-all${button.isActive ? " bg-sky-200/20 cursor-pointer" : ""}`} href={button.path}>
+              {button.label}
+            </a>
+          ))}
+        </div>
+        <a className="p-4 rounded-full bg-sky-200/20 flex items-center justify-center fixed top-2 right-2 cursor-pointer hover:bg-sky-200/30 backdrop-blur-xl" href="/dashboard">
+          <CircleUserRound height="40" width="40" />
+        </a>
+    </nav>
+  )
+}
+
+export default Nav
